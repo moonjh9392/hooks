9f999eb032cf61a4a68c29e61bebbda68a903c0a
moon <moonjh9392@gmail.com>
2022-12-29 19:26:04 +0900

    callback

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import UseMemo from './components/UseMemo';
+import UseCallback from './components/UseCallback';
 
 function App() {
   return (
     <>
       <UseMemo />
+      <UseCallback />
     </>
   );
 }
